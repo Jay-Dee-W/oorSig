0e1d0808d2db7b7b0dc786f23db8e378d095698d
gitstart <gitstart@users.noreply.github.com>
2023-03-27 18:57:40 +0000

    git-slice:2872e2b2af14b67da9146d8a32a82be2eb5e1d8c

--- src/next-env.d.ts
+++ src/next-env.d.ts
@@ -5,23 +5,22 @@
 // see https://nextjs.org/docs/basic-features/typescript for more information.
 
 declare namespace NodeJS {
-    interface ProcessEnv {
-      // NextJS env variables
-      readonly NODE_ENV: 'development' | 'production' | 'test';
-  
-      // Custom env variables
-      readonly NEXT_PUBLIC_GRAPHQL_URL: string; // this is the line you want
-      readonly BACKEND_URL: string;
-      readonly GOOGLE_CLIENT_ID: string;
-      readonly GOOGLE_REDIRECT_URI: string;
-    }
+  interface ProcessEnv {
+    // NextJS env variables
+    readonly NODE_ENV: 'development' | 'production' | 'test';
+
+    // Custom env variables
+    readonly NEXT_PUBLIC_GRAPHQL_URL: string; // this is the line you want
+    readonly BACKEND_URL: string;
+    readonly GOOGLE_CLIENT_ID: string;
+    readonly GOOGLE_REDIRECT_URI: string;
   }
-  
-  declare module 'querystring' {
-    interface ParsedUrlQuery {
-      auth_error?: string;
-      ticketCodePrefix: string;
-      ticketCode: string;
-    }
+}
+
+declare module 'querystring' {
+  interface ParsedUrlQuery {
+    auth_error?: string;
+    ticketCodePrefix: string;
+    ticketCode: string;
   }
-  
+}
