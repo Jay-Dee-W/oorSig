c06f517be98fee556d75200eaf50d0beacfdc66f
wajihaNiazi <waijehaniazi204@gmail.com>
2023-04-07 15:50:03 +0430

    create spinner atom

---
+++ src/atoms/Spinner.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { SystemProps, x } from '@xstyled/emotion';
+
+interface SpinnerProps extends SystemProps {
+  active: boolean;
+}
+
+export const Spinner: React.FC<SpinnerProps> = ({ active= true , ...systemProps }) => {
+  if (!active) return null;
+  return (
+    <x.div display="flex" justifyContent="center" alignItems="center">
+      <x.div
+        display="inline-block"
+        w={8}
+        h={8}
+        border={3}
+        borderColor={'white'}
+        borderTopColor={'primary-600'}
+        borderRadius="full"
+        animation="spin"
+        {...systemProps}
+      />
+    </x.div>
+  );
+};
