d86caf04810567d6757dc8b2a6d871990df38c2f
AqidaHaidari <aghida55@gmail.com>
2023-04-11 11:30:34 +0430

    change px to rem

--- src/atoms/Alert.tsx
+++ src/atoms/Alert.tsx
@@ -47,24 +47,24 @@ export const Alert: React.FC<AlertProps> = ({
       flexDirection="row"
       justifyContent="center"
       alignItems="center"
-      padding="17px 18px"
-      gap="10px"
+      padding="1.0625rem 1.125rem"
+      gap="0.625rem"
       position="absolute"
-      w="368px"
-      left="20px"
-      top="20px"
+      w="23rem"
+      left="1.25rem"
+      top="1.25rem"
       bg={colorsForVariant[variant].bg}
       color={colorsForVariant[variant].text}
-      borderRadius="4px"
+      borderRadius="0.25rem"
     >
       <x.span
-        w="258px"
-        h="24px"
+        w="16.125rem"
+        h="1.5rem"
         fontFamily="Inter"
         fontStyle="normal"
         fontWeight="400"
-        fontSize="16px"
-        lineHeight="24px"
+        fontSize="1rem"
+        lineHeight="1.5rem"
         flex="1"
         overflow="hidden"
         textOverflow="ellipsis"
@@ -72,8 +72,8 @@ export const Alert: React.FC<AlertProps> = ({
         {message}
       </x.span>
       <x.span
-        h="12px"
-        w="12px"
+        h="0.75rem"
+        w="0.75rem"
         flex="none"
         cursor="pointer"
         onClick={handleClick}
