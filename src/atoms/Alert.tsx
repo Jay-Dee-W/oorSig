634c13733c23954327c765de04b958d9432b973b
AqidaHaidari <aghida55@gmail.com>
2023-04-12 16:06:55 +0430

    edited svg

--- src/atoms/Alert.tsx
+++ src/atoms/Alert.tsx
@@ -83,8 +83,8 @@ export const Alert: React.FC<AlertProps> = ({
         onClick={handleClick}
       >
         <x.svg
-          width="12"
-          height="12"
+          w="12"
+          h="12"
           viewBox="0 0 12 12"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
