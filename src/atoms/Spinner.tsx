3b9fdb424ffb4a2a9341387136d7bd96daf02afb
wajihaNiazi <waijehaniazi204@gmail.com>
2023-04-12 09:55:49 +0430

    resolve comment

--- src/atoms/Spinner.tsx
+++ src/atoms/Spinner.tsx
@@ -2,12 +2,37 @@ import React from 'react';
 
 import { SystemProps, keyframes, x } from '@xstyled/emotion';
 
+const WidthHeightSize = {
+  xs: {
+    w: 12,
+    h: 12,
+  },
+  sm: {
+    w: 24,
+    h: 24,
+  },
+  md: {
+    w: 30,
+    h: 30,
+  },
+  lg: {
+    w: 36,
+    h: 36,
+  },
+  xl: {
+    w: 42,
+    h: 42,
+  },
+};
+
 interface SpinnerProps extends SystemProps {
-  active: boolean;
+  active?: boolean;
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 }
 
 export const Spinner: React.FC<SpinnerProps> = ({
   active = true,
+  size = 'md',
   ...systemProps
 }) => {
   if (!active) return null;
@@ -15,8 +40,8 @@ export const Spinner: React.FC<SpinnerProps> = ({
     <x.div display="flex" justifyContent="center" alignItems="center">
       <x.div
         display="inline-block"
-        w={30}
-        h={30}
+        w={WidthHeightSize[size].w || systemProps}
+        h={WidthHeightSize[size].w || systemProps}
         border={3}
         borderColor={'white'}
         borderTopColor="#C8C8C8"
