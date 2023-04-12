8018f62259650fa43fa36457287a3ccb8226c51a
AqidaHaidari <aghida55@gmail.com>
2023-04-12 16:18:45 +0430

    solve format issue

--- src/atoms/Alert.tsx
+++ src/atoms/Alert.tsx
@@ -5,17 +5,17 @@ const colorsForVariant = {
   danger: {
     bg: '#F3D8DB',
     text: '#7A282B',
-    iconColor:'#7A282B'
+    iconColor: '#7A282B',
   },
   success: {
     bg: '#D6E6DF',
     text: '#265035',
-    iconColor: '#265035'
+    iconColor: '#265035',
   },
   info: {
     bg: '#44494B',
     text: 'white',
-    iconColor: '#FEFEFF'
+    iconColor: '#FEFEFF',
   },
 };
 
@@ -75,27 +75,27 @@ export const Alert: React.FC<AlertProps> = ({
         {message}
       </x.span>
       {onDismiss && (
-      <x.span
-        h="0.75rem"
-        w="0.75rem"
-        flex="none"
-        cursor="pointer"
-        onClick={handleClick}
-      >
-        <x.svg
-          w="12"
-          h="12"
-          viewBox="0 0 12 12"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
+        <x.span
+          h="0.75rem"
+          w="0.75rem"
+          flex="none"
+          cursor="pointer"
+          onClick={handleClick}
         >
-          <path
-            d="M12 1.20857L10.7914 0L6 4.79143L1.20857 0L0 1.20857L4.79143 6L0 10.7914L1.20857 12L6 7.20857L10.7914 12L12 10.7914L7.20857 6L12 1.20857Z"
-            fill={colorsForVariant[type].iconColor}
-          />
-        </x.svg>
-      </x.span>
-        )}
+          <x.svg
+            w="12"
+            h="12"
+            viewBox="0 0 12 12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12 1.20857L10.7914 0L6 4.79143L1.20857 0L0 1.20857L4.79143 6L0 10.7914L1.20857 12L6 7.20857L10.7914 12L12 10.7914L7.20857 6L12 1.20857Z"
+              fill={colorsForVariant[type].iconColor}
+            />
+          </x.svg>
+        </x.span>
+      )}
     </x.div>
   );
 };
