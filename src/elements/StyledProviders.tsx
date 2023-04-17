b97bc17b9359b494f2150b219be311d73ec51c81
MUHAWENIMANA Janvier <janviermuhawenimana@gmail.com>
2023-04-17 15:52:07 +0200

    Add Typography atom, fontSize and lineHeight

--- src/elements/StyledProviders.tsx
+++ src/elements/StyledProviders.tsx
@@ -9,6 +9,10 @@ export const GlobalStyle = createGlobalStyle`
   a {
     text-decoration: none;
   }
+  body {
+    background-color: #1C1E1F;
+    color: white
+  }
 `;
 
 const theme = {
@@ -33,6 +37,30 @@ const theme = {
     ['green-200']: '#27AE60',
     ['green-300']: '#219653',
     ['green-400']: '#106C37',
+  },
+  fontSizes: {
+    xs: '0.75rem',
+    sm: '0.875rem',
+    base: '1rem',
+    lg: '1.125rem',
+    xl: '1.25rem',
+    '2xl': '1.5rem',
+    '3xl': '1.875rem',
+    '4xl': '2.25rem',
+    '5xl': '3rem',
+    '6xl': '3.75rem',
+  },
+  lineHeights: {
+    xs: '1rem',
+    sm: '1.25rem',
+    base: '1.5rem',
+    lg: '1.75rem',
+    xl: '1.75rem',
+    '2xl': '2rem',
+    '3xl': '2.25rem',
+    '4xl': '2.5rem',
+    '5xl': '1',
+    '6xl': '1',
   },
 };
 
