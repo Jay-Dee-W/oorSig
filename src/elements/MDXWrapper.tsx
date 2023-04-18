f4c3cad8b48e453a8af7acde0edc90220c77d092
gitstart <gitstart@users.noreply.github.com>
2023-04-18 14:12:29 +0000

    git-slice:6412d82e1a14092d8db1268f3f6e9282461bf9df

---
+++ src/elements/MDXWrapper.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import styled, { x } from '@xstyled/emotion';
+
+interface Props {
+  children?: React.ReactNode;
+}
+
+const Wrapper = styled(x.div)`
+  font-size: 1rem;
+  padding: 1.5rem;
+  background-color: #fff;
+  min-height: 100vh;
+  > p,
+  > h1,
+  > h2,
+  > h3,
+  > h4,
+  > h5,
+  > h6,
+  ul,
+  ol,
+  li {
+    color: #24292f;
+    font-size: 1rem;
+    font-weight: 500;
+    margin: 1rem 0;
+  }
+  > div {
+    margin: 1rem 0;
+  }
+  > p,
+  li {
+    margin: 0 0 0.5rem 0;
+    font-weight: normal;
+  }
+  p {
+    code {
+      color: #24292f;
+      border-radius: 0.4rem;
+      background-color: #f6f8fa;
+      padding: 0.2rem 0.4rem;
+    }
+  }
+  > h1 {
+    font-size: 2.5rem;
+  }
+  > h2 {
+    font-size: 1.5rem;
+  }
+  > h3 {
+    font-size: 1.2rem;
+  }
+  button {
+    margin: 0 0.2rem;
+  }
+  pre {
+    padding: 0.75rem;
+    margin: 1.5rem 0;
+    border-radius: 0.5rem;
+    background-color: #f6f8fa;
+    overflow: auto;
+    code {
+      color: #24292f;
+      .function,
+      .operator {
+        color: #cf222e;
+      }
+      .punctuation {
+        color: #24292f;
+      }
+      .string {
+        color: #0a3069;
+      }
+      .variable {
+        color: #116329;
+      }
+    }
+  }
+  ul,
+  ol {
+    padding-left: 1rem;
+  }
+  ul {
+    list-style: disc;
+  }
+  ol {
+    list-style: decimal;
+  }
+`;
+
+// This wrapper can be used to wrap mdx file contents to add styling as xsytled assumes no styling
+// to any component
+export const MDXWrapper = ({ children }: Props) => (
+  <Wrapper>{children}</Wrapper>
+);
