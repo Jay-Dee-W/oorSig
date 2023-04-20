c46d31750a3ad948c4bddbb94c6a38a01c3b8696
wajihaNiazi <waijehaniazi204@gmail.com>
2023-04-20 09:35:54 +0430

    logo

---
+++ src/atoms/Navigation.tsx
@@ -0,0 +1,20 @@
+import { Logo } from './Logo';
+import { x } from '@xstyled/emotion';
+import Link from 'next/link';
+
+interface NavigationProps {
+  children: React.ReactNode;
+}
+
+export const Navigation: React.FC<NavigationProps> = ({ children }) => (
+  <x.div bg="#2C2F30" w="17.82125rem" color="#ffffff">
+    <Link href="/home">
+      <x.div cursor="pointer" px={2}>
+        <Logo w="16.50rem"/>
+      </x.div>
+    </Link>
+    <x.div display="flex" flexDirection="column">
+      {children}
+    </x.div>
+  </x.div>
+);
---
+++ src/domains/sidebar/Sidebar.tsx
@@ -0,0 +1,9 @@
+import { x} from '@xstyled/emotion';
+
+import { Navigation } from '@atoms/Navigation';
+
+export const Sidebar: React.FC = () => {
+  return (
+    <Navigation >test</Navigation>
+  );
+}
