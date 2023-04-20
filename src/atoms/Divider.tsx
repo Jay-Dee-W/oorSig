439965ccd0c21f9ebd929d048032319ed4e9d4e6
wajihaNiazi <waijehaniazi204@gmail.com>
2023-04-20 12:22:54 +0430

    divder

---
+++ src/atoms/Divider.tsx
@@ -0,0 +1,11 @@
+import { x } from '@xstyled/emotion';
+
+export const Divider: React.FC = ({ ...spaceProps }) => {
+  return (
+    <x.div display="relative" w="100%" {...spaceProps}>
+      <x.div display="absolute" flex alignItems="center">
+        <x.div w="full" borderTopWidth={1} borderColor="gray-300" />
+      </x.div>
+    </x.div>
+  );
+};
