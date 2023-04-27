01b69c202ee2173292702d87229a5465312c79c0
FaridQattali <eng.faridqattali@gmail.com>
2023-04-27 13:08:56 +0430

    fix: remove unused import

--- src/domains/Home.tsx
+++ src/domains/Home.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import { Input } from '@oorsig/atoms';
-import { SearchIcon } from '@oorsig/assets/images/icons';
 export const Home: React.FC = () => {
   return (
     <div className="App">
       <h1>GITSTART HOME PAGE</h1>
-      <Input icon={<SearchIcon />} />
     </div>
   );
 };
