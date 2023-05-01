ea32a347c5cfaac01896e30ce7aee590b02498bc
wajihaNiazi <waijehaniazi204@gmail.com>
2023-05-01 09:37:43 +0430

    resolve

--- src/atoms/Divider.tsx
+++ src/atoms/Divider.tsx
@@ -1,10 +1,17 @@
-import { x } from '@xstyled/emotion';
+import { x , SystemProps} from '@xstyled/emotion';
 
-export const Divider: React.FC = ({ ...spaceProps }) => {
+interface DividerProps extends SystemProps {
+  borderColor?:'gray-300' | 'gray-200'| 'gray-100' ,
+}
+
+export const Divider: React.FC<DividerProps> = ({
+  borderColor = "gray-100",
+  ...spaceProps
+}) => {
   return (
     <x.div display="relative" w="100%" {...spaceProps}>
       <x.div display="absolute" flex alignItems="center">
-        <x.div w="full" borderTopWidth={1} borderColor="gray-300" />
+        <x.div w="full" borderTopWidth={1} borderColor={borderColor} />
       </x.div>
     </x.div>
   );
