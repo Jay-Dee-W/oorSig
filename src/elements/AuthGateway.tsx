7b058be320c2ab23c0bb6eaa51389946d2539750
Jay-Dee-W <jaydee.wilson@gmail.com>
2023-05-18 18:54:43 +0200

    change signIn logic

--- src/elements/AuthGateway.tsx
+++ src/elements/AuthGateway.tsx
@@ -1,31 +1,17 @@
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import { useSession } from 'next-auth/react';
+import { useSession, signIn } from 'next-auth/react';
 
 export const AuthGateway: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const router = useRouter();
-  const { data: session } = useSession();
-  const [authLoading, setAuthLoading] = useState(true);
+  const { data: session, status } = useSession();
 
-  useEffect(() => {
-    if (router.asPath !== '/') {
-      setAuthLoading(false);
-      return;
-    }
-    if (!session) {
-      setAuthLoading(false);
-      return;
-    }
-    if (session?.user?.name) {
-      router.replace(`/home`);
-    }
-    setAuthLoading(false);
-  }, [router, session]);
+  if (status === 'loading') {
+    return <p>Loading...</p>;
+  }
 
-  if (authLoading) {
-    return <p>loading</p>;
+  if (!session) {
+    signIn();
+    return null;
   }
 
   return <>{children}</>;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,10 @@
 import Head from 'next/head';
 import type { NextPage } from 'next';
 
-import { useSession, signIn } from 'next-auth/react';
-
 const AuthPage: NextPage = () => {
-  const { data: session } = useSession();
-
-  if (!session) {
-    signIn();
-  }
-
   return (
     <Head>
-      <title>Login | OORSIG</title>
+      <title>OORSIG</title>
     </Head>
   );
 };
