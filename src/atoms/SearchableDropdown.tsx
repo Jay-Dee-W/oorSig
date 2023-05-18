de7d9f2a8c50f7d33fe250907d858c7dade49673
AqidaHaidari <aghida55@gmail.com>
2023-05-18 09:40:02 +0430

    add customization for input search and img size

--- src/atoms/SearchableDropdown.tsx
+++ src/atoms/SearchableDropdown.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { x } from '@xstyled/emotion';
 import { useCombobox } from 'downshift';
-import { Input } from '@atoms/index';
-import { SearchIcon } from '@atoms/index';
+import { Input } from '@atoms/Input';
+import { SearchIcon } from '@atoms/SearchIcon';
 
 interface SelectOption {
   label: string;
@@ -14,12 +14,16 @@ interface SelectProps {
   options: SelectOption[];
   placeholder?: string;
   label?: string;
+  imgSize?: string;
+  isSearchable?: boolean;
 }
 
 export const SearchableDropdown: React.FC<SelectProps> = ({
   options,
   label = 'Searchable Dropdown',
   placeholder = 'Search option',
+  imgSize = '1.875rem',
+  isSearchable = false,
 }) => {
   function getOptionsFilter(inputValue: string) {
     const lowerCasedInputValue = inputValue.toLowerCase();
@@ -35,7 +39,6 @@ export const SearchableDropdown: React.FC<SelectProps> = ({
 
   const [items, setItems] = useState(options);
   const {
-    isOpen,
     getLabelProps,
     getMenuProps,
     getInputProps,
@@ -57,49 +60,59 @@ export const SearchableDropdown: React.FC<SelectProps> = ({
           color="white"
           w="fit"
           borderRadius="0.5rem 0.5rem 0 0"
-          px="0.7rem"
-          py="0.7rem"
+          px="0.625rem"
+          py="0.577rem"
           {...getLabelProps()}
         >
           {label}
         </x.label>
-        <x.div p="0.625rem" bg="gray-300" borderRadius="0 0 0.5rem 0.5rem">
-          <Input
-            borderColor="gray-50"
-            placeholder={placeholder}
-            icon={<SearchIcon />}
-            {...getInputProps()}
-          />
-          <x.ul bg="gray-300" color="white" {...getMenuProps()}>
-            {isOpen &&
-              items.map((item, index) => (
-                <x.li
-                  alignContent="center"
-                  py="0.7rem"
-                  px="0.7rem"
-                  bg={highlightedIndex === index ? 'gray-200' : 'transparent'}
-                  borderRadius={highlightedIndex === index ? '0.367rem' : ''}
+        <x.div bg="gray-300" p="0.625rem" borderRadius="0 0 0.5rem 0.5rem">
+          {isSearchable && (
+            <Input
+              icon={<SearchIcon />}
+              placeholder={placeholder}
+              borderColor="gray-50"
+              {...getInputProps()}
+            />
+          )}
+          <x.ul
+            bg="gray-300"
+            color="white"
+            pt={isSearchable && '0.625rem'}
+            borderRadius="0 0 0.5rem 0.5rem"
+            {...getMenuProps()}
+          >
+            {items.map((item, index) => (
+              <x.li
+                alignContent="center"
+                py="0.625rem"
+                px="0.5rem"
+                bg={highlightedIndex === index ? 'gray-200' : 'transparent'}
+                borderRadius={highlightedIndex === index ? '0.367rem' : ''}
+                display="flex"
+                flexDirection="row"
+                cursor="pointer"
+                key={`${index}`}
+                {...getItemProps({ item, index })}
+              >
+                {item.imgSrc && (
+                  <x.img
+                    src={item.imgSrc}
+                    borderRadius="0.25rem"
+                    w={imgSize}
+                    h={imgSize}
+                  />
+                )}
+                <x.span
+                  pl="0.5rem"
+                  fontSize="base"
                   display="flex"
-                  flexDirection="row"
-                  cursor="pointer"
-                  key={`${index}`}
-                  {...getItemProps({ item, index })}
+                  alignItems="center"
                 >
-                  {item.imgSrc ? (
-                    <x.img src={item.imgSrc} w="1.875rem" h="1.875rem" />
-                  ) : (
-                    ''
-                  )}
-                  <x.span
-                    pl="0.5rem"
-                    fontSize="base"
-                    display="flex"
-                    alignItems="center"
-                  >
-                    {item.label}
-                  </x.span>
-                </x.li>
-              ))}
+                  {item.label}
+                </x.span>
+              </x.li>
+            ))}
           </x.ul>
         </x.div>
       </x.div>
