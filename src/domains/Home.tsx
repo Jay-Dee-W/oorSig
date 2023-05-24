3d9458e527ad7c54b2456e970ca02a2dadb175d6
wajihaNiazi <waijehaniazi204@gmail.com>
2023-05-24 08:29:52 +0430

    resolved

--- src/domains/Home.tsx
+++ src/domains/Home.tsx
@@ -1,22 +1,9 @@
 import React from 'react';
-import { SearchableDropdown } from '@atoms/index';
-
 
 export const Home: React.FC = () => {
   return (
     <div className="App">
       <h1>GITSTART HOME PAGE</h1>
-      <SearchableDropdown
-    options={[
-      { label: 'Option 1', value: 'option1', imgSrc: '/profile.jpeg' },
-      { label: 'Option 2', value: 'option2', imgSrc: '/profile.jpeg' },
-      { label: 'Option 3', value: 'option3', imgSrc: '/profile.jpeg'  },
-    ]}
-    placeholder="Search Team"
-    label="Select Team"
-    imgSize='2rem'
-    isSearchable={true}
-  />
     </div>
   );
 };
