794b62ffc3adb58a9f190cfa383f5d1002d8f666
Jay-Dee-W <jaydee.wilson@gmail.com>
2023-05-26 10:41:45 +0200

    fix ts error in relay enviroment

--- src/relay/environment.tsx
+++ src/relay/environment.tsx
@@ -8,8 +8,14 @@ import {
   authMiddleware,
 } from 'react-relay-network-modern';
 import React from 'react';
+import { Session } from 'next-auth';
 import { getSession } from 'next-auth/react';
 
+
+interface session extends Session{
+  access_token?: string | null
+  
+}
 export const LOCAL_STORAGE_RELAY_RECORDS_KEY = 'relay_records';
 
 export const environment = new Environment({
@@ -24,8 +30,8 @@ export const environment = new Environment({
     authMiddleware({
       // this middleware automatically adds 'Bearer ' at the start of the Authorization header
       token: async () => {
-        const session = await getSession();
-        return session?.access_token;
+        const session:session|null = await getSession();
+        return session?.access_token ?? "";
       },
     }),
     next => async req => {
