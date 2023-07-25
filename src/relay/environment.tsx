72c0f1c16bf47288a2fa8b6ed79fa4a84dc0bc8d
wajihaNiazi <waijehaniazi204@gmail.com>
2023-07-25 16:32:48 +0430

    load profile data

--- src/relay/environment.tsx
+++ src/relay/environment.tsx
@@ -27,11 +27,10 @@ export const environment = new Environment({
     }),
     authMiddleware({
       // this middleware automatically adds 'Bearer ' at the start of the Authorization header
-      token: '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',
-      //   async () => {
-      //   const session: ISession | null = await getSession();
-      //   return session?.accessToken ?? '';
-      // },
+      token: async () => {
+        const session: ISession | null = await getSession();
+        return session?.accessToken ?? '';
+      },
     }),
     next => async req => {
       const res = await next(req);
